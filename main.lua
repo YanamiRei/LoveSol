a5a5854199218637188a6de0a055bcae2903076f
YanamiRei <rei.tteiimasu@gmail.com>
2026-05-14 14:08:57 +0530

    Golf: add 9-slice Dialog boxes and buttons

--- main.lua
+++ main.lua
@@ -5,16 +5,15 @@ CARD_DIMENSIONS = { x = 162, y = 204 }
 CARD_GAP = { x = 30, y = 40 }
 
 CARD_SPRITE_DIMENSIONS = { x = 27, y = 34 }
+DIALOG_BOX_SPRITE_DIMENSION = 3
 
 MAX_SCALE = 1
 BG_SCALE = 4
+BASE_FONT_SIZE = 64
 
 Scale = 1
 
-CardSpriteScale = {
-	x = CARD_DIMENSIONS.x / CARD_SPRITE_DIMENSIONS.x * Scale,
-	y = CARD_DIMENSIONS.y / CARD_SPRITE_DIMENSIONS.y * Scale,
-}
+PixelPerfectScale = CARD_DIMENSIONS.x / CARD_SPRITE_DIMENSIONS.x * Scale
 
 function math.Clamp(val, lower, upper)
 	if lower > upper then
@@ -62,20 +61,33 @@ function DistributeDeck(deck)
 end
 
 function love.load()
-	Font = love.graphics.newFont(16)
+	DialogOpen = false
+	LoadFont()
 	love.graphics.setDefaultFilter("nearest", "nearest")
-	CardSprites = {}
+	Sprites = {}
 
 	local suits = { C = "Clubs", S = "Spades", H = "Hearts", D = "Diamonds" }
 	for abbr, name in pairs(suits) do
 		for i = 1, 13 do
 			local card = abbr .. i
-			CardSprites[card] = love.graphics.newImage("Sprites/" .. name .. "/" .. card .. ".png")
+			Sprites[card] = love.graphics.newImage("Sprites/" .. name .. "/" .. card .. ".png")
 		end
 	end
-	CardSprites.back0 = love.graphics.newImage("Sprites/Backs/back_0.png")
-	CardSprites.backX = love.graphics.newImage("Sprites/Backs/back_x.png")
-	CardSprites.undo = love.graphics.newImage("Sprites/undo.png")
+	Sprites.back0 = love.graphics.newImage("Sprites/Backs/back_0.png")
+	Sprites.backX = love.graphics.newImage("Sprites/Backs/back_x.png")
+	Sprites.undo = love.graphics.newImage("Sprites/undo.png")
+	Sprites.box = {}
+	Sprites.box.corner = love.graphics.newImage("Sprites/DialogBox/corner.png")
+	Sprites.box.edge = love.graphics.newImage("Sprites/DialogBox/edge.png")
+	Sprites.box.center = love.graphics.newImage("Sprites/DialogBox/center.png")
+	Sprites.button = {}
+	Sprites.button.corner = love.graphics.newImage("Sprites/Button/corner.png")
+	Sprites.button.edge = love.graphics.newImage("Sprites/Button/edge.png")
+	Sprites.button.center = love.graphics.newImage("Sprites/Button/center.png")
+	Sprites.buttonHot = {}
+	Sprites.buttonHot.corner = love.graphics.newImage("Sprites/Button/cornerHot.png")
+	Sprites.buttonHot.edge = love.graphics.newImage("Sprites/Button/edgeHot.png")
+	Sprites.buttonHot.center = love.graphics.newImage("Sprites/Button/centerHot.png")
 
 	Deck = GenrateDeck()
 	Tableu = DistributeDeck(Deck)
@@ -85,15 +97,20 @@ function love.load()
 	SetupDrawPositions()
 	TopCardsPosition = GetTopCardsPosition()
 
-	CardSprites.background = love.graphics.newImage("Sprites/table.png")
-	CardSprites.background:setWrap("repeat", "repeat")
-	BackgroundQuad = love.graphics.newQuad(0, 0, Ww / BG_SCALE, Wh / BG_SCALE, CardSprites.background:getDimensions())
+	Sprites.background = love.graphics.newImage("Sprites/table.png")
+	Sprites.background:setWrap("repeat", "repeat")
+	BackgroundQuad = love.graphics.newQuad(0, 0, Ww / BG_SCALE, Wh / BG_SCALE, Sprites.background:getDimensions())
+end
+
+function LoadFont()
+	Font = love.graphics.newFont("Fonts/PixelSans.ttf", math.Clamp(BASE_FONT_SIZE * Scale, 1, 1000))
+	love.graphics.setFont(Font)
 end
 
 function love.update()
 	lastMouseState = currentMouseState
 	currentMouseState = love.mouse.isDown(1)
-	if lastMouseState and not currentMouseState then
+	if lastMouseState and not currentMouseState and not DialogOpen then
 		local mx, my = love.mouse.getPosition()
 		Click(mx, my)
 	end
@@ -172,19 +189,22 @@ function GetTopCardsPosition()
 end
 
 function love.draw()
-	love.graphics.draw(CardSprites.background, BackgroundQuad, 0, 0, 0, BG_SCALE, BG_SCALE)
+	love.graphics.draw(Sprites.background, BackgroundQuad, 0, 0, 0, BG_SCALE, BG_SCALE)
 	DrawTableu()
 	DrawBottom()
+	if DialogOpen then
+		DrawDialog("You Lose!", "Play again", function()
+			DialogOpen = false
+		end)
+	end
 end
 
 function love.resize()
-	CardSpriteScale = {
-		x = CARD_DIMENSIONS.x / CARD_SPRITE_DIMENSIONS.x * Scale,
-		y = CARD_DIMENSIONS.y / CARD_SPRITE_DIMENSIONS.y * Scale,
-	}
 	SetupDrawPositions()
+	PixelPerfectScale = CARD_DIMENSIONS.x / CARD_SPRITE_DIMENSIONS.x * Scale
 	TopCardsPosition = GetTopCardsPosition()
-	BackgroundQuad = love.graphics.newQuad(0, 0, Ww / BG_SCALE, Wh / BG_SCALE, CardSprites.background:getDimensions())
+	BackgroundQuad = love.graphics.newQuad(0, 0, Ww / BG_SCALE, Wh / BG_SCALE, Sprites.background:getDimensions())
+	LoadFont()
 end
 
 function SetupDrawPositions()
@@ -225,10 +245,10 @@ function DrawCard(x, y, card)
 	if not card then
 		return
 	end
-	local sprite = CardSprites[card]
+	local sprite = Sprites[card]
 	love.graphics.push()
-	love.graphics.scale(CardSpriteScale.x, CardSpriteScale.y)
-	love.graphics.draw(sprite, x / CardSpriteScale.x, y / CardSpriteScale.y)
+	love.graphics.scale(PixelPerfectScale, PixelPerfectScale)
+	love.graphics.draw(sprite, x / PixelPerfectScale, y / PixelPerfectScale)
 	love.graphics.pop()
 end
 
@@ -244,24 +264,24 @@ end
 
 function DrawBottom()
 	-- Stock
-	local cardBackSprite = #Deck ~= 0 and CardSprites.back0 or CardSprites.backX
+	local cardBackSprite = #Deck ~= 0 and Sprites.back0 or CardSprites.backX
 	love.graphics.push()
-	love.graphics.scale(CardSpriteScale.x, CardSpriteScale.y)
+	love.graphics.scale(PixelPerfectScale, PixelPerfectScale)
 	love.graphics.draw(
 		cardBackSprite,
-		DrawPositions.Stock.x / CardSpriteScale.x,
-		DrawPositions.Stock.y / CardSpriteScale.y
+		DrawPositions.Stock.x / PixelPerfectScale,
+		DrawPositions.Stock.y / PixelPerfectScale
 	)
 	love.graphics.pop()
 
 	-- Undo
-	local cardBackSprite = CardSprites.undo
+	local cardBackSprite = Sprites.undo
 	love.graphics.push()
-	love.graphics.scale(CardSpriteScale.x, CardSpriteScale.y)
+	love.graphics.scale(PixelPerfectScale, PixelPerfectScale)
 	love.graphics.draw(
 		cardBackSprite,
-		DrawPositions.Undo.x / CardSpriteScale.x,
-		DrawPositions.Undo.y / CardSpriteScale.y
+		DrawPositions.Undo.x / PixelPerfectScale,
+		DrawPositions.Undo.y / PixelPerfectScale
 	)
 	love.graphics.pop()
 
@@ -270,5 +290,155 @@ function DrawBottom()
 	for i, card in ipairs(Foundation) do
 		local x = DrawPositions.Foundation.x + ((i - 1) * CARD_GAP.x * Scale)
 		DrawCard(x, y, card)
+	end
+end
+
+function DrawDialog(text, buttonText, onPressed)
+	local spriteSize = DIALOG_BOX_SPRITE_DIMENSION * PixelPerfectScale
+
+	local totalHeight = math.max(
+		(((2 * MARGIN) + MARGIN + MARGIN + MARGIN + (2 * MARGIN)) * Scale)
+			+ Font:getHeight(text)
+			+ Font:getHeight(buttonText),
+		2 * spriteSize
+	)
+	local totalWidth = math.max(
+		math.max(
+			(((2 * MARGIN) + (2 * MARGIN)) * Scale) + Font:getWidth(text),
+			(((2 * MARGIN) + MARGIN + MARGIN + (2 * MARGIN)) * Scale) + Font:getWidth(buttonText)
+		),
+		2 * spriteSize
+	)
+
+	local x = ((Ww / 2) - (totalWidth / 2))
+	local y = ((Wh / 2) - (totalHeight / 2))
+
+	drawBox(Sprites.box, x, y, totalWidth, totalHeight)
+
+	love.graphics.print(
+		{ { 0, 0, 0, 1 }, text },
+		x + (totalWidth / 2) - Font:getWidth(text) / 2,
+		y + (2 * MARGIN * Scale)
+	)
+
+	local buttonX = x + (2 * MARGIN * Scale)
+	local buttonY = y + totalHeight - ((MARGIN + (2 * MARGIN)) * Scale) - Font:getHeight(buttonText)
+
+	drawButton(buttonText, buttonX, buttonY, onPressed)
+end
+
+function drawBox(spriteSet, baseX, baseY, totalWidth, totalHeight)
+	local x = baseX / PixelPerfectScale
+	local y = baseY / PixelPerfectScale
+
+	local baseSpriteSize = DIALOG_BOX_SPRITE_DIMENSION
+	local spriteSize = baseSpriteSize * PixelPerfectScale
+
+	love.graphics.push()
+	love.graphics.scale(PixelPerfectScale, PixelPerfectScale)
+
+	-- Corners
+	love.graphics.draw(spriteSet.corner, x, y)
+	love.graphics.draw(
+		spriteSet.corner,
+		x + (baseSpriteSize / 2) + (totalWidth / PixelPerfectScale) - baseSpriteSize,
+		y + (baseSpriteSize / 2),
+		math.pi / 2,
+		1,
+		1,
+		baseSpriteSize / 2,
+		baseSpriteSize / 2
+	)
+	love.graphics.draw(
+		spriteSet.corner,
+		x + (baseSpriteSize / 2) + (totalWidth / PixelPerfectScale) - baseSpriteSize,
+		y + (baseSpriteSize / 2) + (totalHeight / PixelPerfectScale) - baseSpriteSize,
+		math.pi,
+		1,
+		1,
+		baseSpriteSize / 2,
+		baseSpriteSize / 2
+	)
+
+	love.graphics.draw(
+		spriteSet.corner,
+		x + (baseSpriteSize / 2),
+		y + (baseSpriteSize / 2) + (totalHeight / PixelPerfectScale) - baseSpriteSize,
+		-math.pi / 2,
+		1,
+		1,
+		baseSpriteSize / 2,
+		baseSpriteSize / 2
+	)
+
+	-- Edges
+
+	local EdgeSize = {
+		x = (totalWidth - (2 * spriteSize)),
+		y = (totalHeight - (2 * spriteSize)),
+	}
+
+	local edgeScale = {
+		x = EdgeSize.x / spriteSize,
+		y = EdgeSize.y / spriteSize,
+	}
+
+	love.graphics.draw(spriteSet.edge, x + baseSpriteSize, y, 0, edgeScale.x, 1, 0, 0)
+	love.graphics.draw(
+		spriteSet.edge,
+		x + (baseSpriteSize / 2) + baseSpriteSize + EdgeSize.x / PixelPerfectScale,
+		y + (baseSpriteSize * edgeScale.y / 2) + baseSpriteSize,
+		math.pi / 2,
+		edgeScale.y,
+		1,
+		baseSpriteSize / 2,
+		baseSpriteSize / 2
+	)
+	love.graphics.draw(
+		spriteSet.edge,
+		x + (baseSpriteSize * edgeScale.x / 2) + baseSpriteSize,
+		y + (baseSpriteSize / 2) + baseSpriteSize + EdgeSize.y / PixelPerfectScale,
+		math.pi,
+		edgeScale.x,
+		1,
+		baseSpriteSize / 2,
+		baseSpriteSize / 2
+	)
+	love.graphics.draw(
+		spriteSet.edge,
+		x + (baseSpriteSize / 2),
+		y + (baseSpriteSize * edgeScale.y / 2) + baseSpriteSize,
+		-math.pi / 2,
+		edgeScale.y,
+		1,
+		baseSpriteSize / 2,
+		baseSpriteSize / 2
+	)
+
+	-- Center
+
+	love.graphics.draw(spriteSet.center, x + baseSpriteSize, y + baseSpriteSize, 0, edgeScale.x, edgeScale.y)
+
+	love.graphics.pop()
+end
+
+function drawButton(text, x, y, onPressed)
+	local width = 2 * MARGIN * Scale + Font:getWidth(text)
+	local height = 2 * MARGIN * Scale + Font:getHeight(text)
+	local mx, my = love.mouse.getPosition()
+	local sprite = Sprites.button
+	local hot = mx > x and mx < x + width and my > y and my < y + height
+	if hot then
+		sprite = Sprites.buttonHot
+	end
+	drawBox(sprite, x, y, width, height)
+	love.graphics.print(
+		{ { 0, 0, 0, 1 }, text },
+		x + (width / 2) - (Font:getWidth(text) / 2),
+		y + (height / 2) - (Font:getHeight(text) / 2)
+	)
+
+	if lastMouseState and not currentMouseState and DialogOpen and hot then
+		onPressed()
 	end
 end
